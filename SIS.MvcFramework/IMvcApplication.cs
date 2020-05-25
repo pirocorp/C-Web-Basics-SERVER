b8a468581269a3fbccda8b4d7eccd47a991457db
Zdravko Zdravkov <zdr0@yahoo.com>
2020-05-25 16:36:19 +0200

    Add application conception

---
+++ SIS.MvcFramework/IMvcApplication.cs
@@ -0,0 +1,13 @@
+﻿namespace SIS.MvcFramework
+{
+    using System.Collections.Generic;
+
+    using HTTP;
+
+    public interface IMvcApplication
+    {
+        void Configure(IList<Route> routeTable);
+
+        void ConfigureServices();
+    }
+}
---
+++ SulsApp/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace SulsApp
+{
+    using System.Collections.Generic;
+    using Controllers;
+    using SIS.HTTP;
+    using SIS.MvcFramework;
+
+    public class StartUp : IMvcApplication
+    {
+        public void ConfigureServices()
+        {
+            var db = new ApplicationDbContext();
+            db.Database.EnsureCreated();
+        }
+
+        public void Configure(IList<Route> routeTable)
+        {
+            routeTable.Add(new Route(HttpMethodType.Get, "/", new HomeController().Index));
+            routeTable.Add(new Route(HttpMethodType.Get, "/Users/Login", new UsersController().Login));
+            routeTable.Add(new Route(HttpMethodType.Get, "/Users/Register", new UsersController().Register));
+            routeTable.Add(new Route(HttpMethodType.Get, "/css/bootstrap.min.css", new StaticFilesController().Bootstrap));
+            routeTable.Add(new Route(HttpMethodType.Get, "/css/site.css", new StaticFilesController().Site));
+            routeTable.Add(new Route(HttpMethodType.Get, "/css/reset.css", new StaticFilesController().Reset));
+        }
+    }
+}
